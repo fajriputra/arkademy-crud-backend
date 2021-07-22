e6ec7eb1c574fb96c293d948290022255b028b2d
AL FAJRI PUTRA PRATAMA <72371119+fajriputra@users.noreply.github.com>
2021-07-22 08:45:22 +0700

    Update index.js
    - ganti connection database

--- database/index.js
+++ database/index.js
@@ -4,7 +4,7 @@ const { dbUser, dbPass, dbName } = require("../app/config");
 
 // (3) connect to mongodb
 mongoose.connect(
-  `mongodb://${dbUser}:${dbPass}@cluster0-shard-00-00.emeay.mongodb.net:27017,cluster0-shard-00-01.emeay.mongodb.net:27017,cluster0-shard-00-02.emeay.mongodb.net:27017/${dbName}?ssl=true&replicaSet=atlas-g39jzr-shard-0&authSource=admin&retryWrites=true&w=majority`,
+  `mongodb+srv://${dbUser}:${dbPass}@cluster0.emeay.mongodb.net/${dbName}?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
